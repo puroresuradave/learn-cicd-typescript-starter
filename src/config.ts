a4fc36712494a7565357877048b87b50a4e9c04e
puroresuradave <puroresuradave@gmail.com>
2025-07-14 18:14:12 -0700

    Database lessons

--- src/config.ts
+++ src/config.ts
@@ -21,6 +21,6 @@ export const config: Config = {
     filepathRoot: "./src/assets",
   },
   db: {
-    url: process.env.DATABASE_URL,
+    url: "libsql://notely-db-puroresuradave.aws-us-west-2.turso.io?authToken=eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3NTI0NzQ3MzMsImlkIjoiZTJlYTIxZjQtOGJjNC00ZjI0LTlkMmEtMGRmZDRlNWIyYzlkIiwicmlkIjoiNjQxNzE4NWUtZDM3My00ZWFhLWE0NGQtODMwYzc0NTU3NmM3In0.bQaj4OO3tVMp6X-vAMiJxwova4BopwV_zLWeccp6McelOPlMaYEt9L0pwr3-GWpznY6cUcFX55l3UbqoV4hmCA",
   },
 };
